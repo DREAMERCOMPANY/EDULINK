eec74f929e191278c3800b1bdb23677fa0c357e8
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2025-01-23 21:10:16 -0500

    Adding new features

--- Js/index.js
+++ Js/index.js
@@ -1,8 +1,20 @@
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
+const blurEffect = document.getElementById('blur')
 btn.addEventListener('click', ()=> navToggle())
+
+//Blur function
+blurEffect.addEventListener('click', ()=>{
+  blurEffect.classList.toggle('hidden')
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+  menu.classList.toggle('animate')
+})
+
 // Toggle Mobile Menu
 function navToggle() {
+  blurEffect.classList.toggle('hidden')
   btn.classList.toggle('open')
   menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
